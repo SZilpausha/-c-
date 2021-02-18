fd5f15dd91eff1f77f3d8f004a8f671214333eb4
Svetlana Zilpausha <szilpausha@gmail.com>
2021-02-18 14:02:50 +0000

    vidēja atzīme

--- main.cpp
+++ main.cpp
@@ -28,16 +28,30 @@
 //   }
 //   return 0;
 // }
+// #include <iostream>
+// using namespace std;
+// // no 50 līdz 0
+// int main() {
+//   int starp = 1;
+//   for(int i = 50; i <= 100; i = i + starp++ )
+ 
+//   {
+//     cout << i <<endl;
+  
+//   }
+//   return 0;
+// } 
 #include <iostream>
 using namespace std;
-// no 50 līdz 0
-int main() {
-  int starp = 1;
-  for(int i = 50; i <= 100; i = i + starp++ )
- 
-  {
-    cout << i <<endl;
-  
-  }
-  return 0;
-} 
+int main()
+{
+    int skSk = 0, atz = 0, sum = 0;
+    cin >> skSk;
+    for(int i = 0; i < skSk; i = i + 1)
+    {
+        cin >> atz;
+        sum = sum + atz;
+    }
+    cout << sum / skSk;
+    return 0;
+}
