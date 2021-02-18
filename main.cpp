174abf3821e3eff7b793c018bc5b4288783e7331
Svetlana Zilpausha <szilpausha@gmail.com>
2021-02-18 13:15:36 +0000

    Skaitļus no 50 līdz 100, katru reizi intervālu (starpību) palielinot par 1, piemēram šādi: 50, 51, 53, 56, 60...

--- main.cpp
+++ main.cpp
@@ -1,17 +1,43 @@
+// #include <iostream>
+// using namespace std;
+// // no 1 līdz 100
+// int main() {
+//   for(int i = 1; i < 101; i = i + 1)
+//   {
+//     cout << i <<endl;
+//   }
+//   return 0;
+// }
+// #include <iostream>
+// using namespace std;
+// // no 20 līdz 40
+// int main() {
+//   for(int i = 20; i <= 40; i = i + 2)
+//   {
+//     cout << i <<endl;
+//   }
+//   return 0;
+// }
+// #include <iostream>
+// using namespace std;
+// // no 50 līdz 0
+// int main() {
+//   for(int i = 50; i >= 0; i = i - 1)
+//   {
+//     cout << i <<endl;
+//   }
+//   return 0;
+// }
 #include <iostream>
 using namespace std;
-// Atrodi lielāko no 3 skaitļiem.
+// no 50 līdz 0
 int main() {
-  int x, y, c;
-cout << "Ievadi tris skaitlus intervalā [-10000; 10000] (skaitļiem jābūt atdalītiem ar tukšumsimbolu): ";
-cin >> x >> y >> c;
-int max = x;
-if (y > max) {
-        max = y;
-    }
-    if (c > max) {
-        max = c;
-    }
-    cout << max << endl;
+  int starp = 1;
+  for(int i = 50; i <= 100; i = i + starp++ )
+ 
+  {
+    cout << i <<endl;
+  
+  }
   return 0;
-}
+} 
